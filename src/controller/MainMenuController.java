de2544e775b9e09ac24a5e6c0d978e36a5353602
Ricardo Marcano <Ricardo Marcano@RicardoMarcano>
2016-10-06 23:34:20 -0300

    Implementacion de los TAD completada, falta agregar control sobre la casa, e implementar el servidor con android

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -10,12 +10,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -28,9 +30,11 @@ public class MainMenuController implements Initializable {
 
       @FXML Label date;
       @FXML Label hour;
+      @FXML AnchorPane topPane;
       public Stage primStage;
-      boolean maximized;
-      boolean minimized;
+      Double xs,ys;
+      boolean maximized=false;
+      boolean minimized=false;
             
             
       /**
@@ -40,6 +44,7 @@ public class MainMenuController implements Initializable {
       public void initialize(URL url, ResourceBundle rb) {
             // TODO
             new DigitalClock(date,hour);
+            drag();
       }      
       
        @FXML public void focusin(MouseEvent evt){
@@ -95,13 +100,29 @@ public class MainMenuController implements Initializable {
             Stage stage;
             Button  b = (Button) evt.getSource();
             stage = (Stage) b.getScene().getWindow();
-            if(minimized){
-                  stage.setIconified(false);
-                  minimized = false;
-            }else{
-                  stage.setIconified(true);
-                  minimized = true;
-            }
+            stage.setIconified(true);
       }     
+      
+      public void drag(){
+
+            topPane.setOnMousePressed(new EventHandler<MouseEvent>(){
+                  @Override
+                  public void handle(MouseEvent evt){
+                        xs=evt.getSceneX();
+                        ys=evt.getSceneY();
+                  }
+            });           
+            
+            topPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primStage.setX(event.getScreenX() - xs);
+                primStage.setY(event.getScreenY() - ys);
+            }
+        });
+            
+
+            
+      }
             
 }
